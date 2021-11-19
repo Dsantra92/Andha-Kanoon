bed9418e20f2761afeceeebd97152f54eba2c343
Deeptendu Santra <deeptendu.santra@protonmail.com>
2021-11-19 21:15:02 +0530

    folder structuring is gheyy

--- src/query.jl
+++ src/query.jl
@@ -1,14 +1,21 @@
+module query
+
+export generate_query_string, get_number_of_results_for_query, get_sub_optimal_dates
+
+include("years.jl")
+include("utils.jl")
+
 using HTTP
 using Gumbo
 using Cascadia
 using Dates
-include("utils.jl")
-include("years.jl")
+using .years
+
 
 """
-Generates a search query given starting date and ending dates.
+Returns a query string for a starting date and ending date.
 """
-function generate_query(court_id::String, start_date::Date, end_date::Date)::String
+function generate_query_string(court_id::String, start_date::Date, end_date::Date)::String
     base_url = "https://indiankanoon.org/search/?formInput=doctypes:"
     date1 = Dates.format(start_date, "d-m-Y")
     date2 = Dates.format(end_date, "d-m-Y")
@@ -19,28 +26,33 @@ function generate_query(court_id::String, start_date::Date, end_date::Date)::Str
     return query
 end
 
+
 """
-Returns the actual number of results for the query.
+Returns the actual number of results from the query result.
 """
-function get_number_of_results(html_query_page:: HTMLDocument)::Int
+function get_number_of_results_for_query(html_query_page:: HTMLDocument)::Int
     query_info = nodeText(eachmatch(Selector("b"), html_query_page.root)[1])
 
     no_of_results = parse(Int, split(query_info, " ")[5])
     return no_of_results
 end
 
-# Not recommended to use this function. Just for safety
-function get_number_of_results(query:: String)::Int
-    return get_number_of_results(get_html(query))
+"""
+Returns the actual number of results from the query.
+"""
+function get_number_of_results_for_query(query:: String)::Int
+    return get_number_of_results_for_query(get_html(query))
 end
 
-
-function generate_date_with_gaps(date1::Date, date2::Date, gap::Day, queries::Int)::Vector{Tuple{Date, Date}}
-    date_list = Vector{Tuple{Date, Date}}(undef, queries)
+"""
+Splits the query dates into a vector of sub-optimal dates.
+"""
+function divide_dates(date1::Date, date2::Date, gap::Day, n_queries::Int)::Vector{Tuple{Date, Date}}
+    date_list = Vector{Tuple{Date, Date}}(undef, n_queries)
 
     date_list[1] = (date1, date1 + gap)
 
-    for i in range(2, queries-1)
+    for i in range(2, n_queries-1)
         new_date = date_list[i-1][2] + Day(1)
         date_list[i] = (new_date, new_date + gap)
     end
@@ -48,20 +60,27 @@ function generate_date_with_gaps(date1::Date, date2::Date, gap::Day, queries::In
     return date_list
 end
 
+
 """
-Generates the pseudo-optimal query for a given court.
-To be called only when the query results > 400.
+Splits the dates into a vector of sub-optimal dates depending on the number of results. This function
+expects the user to have checked the inputs before calling this function.
 """
-function get_sub_optimal_query(date1::Date, date2::Date, n_results::Int)::Vector{Tuple{Date, Date}}
+function get_sub_optimal_dates(date1::Date, date2::Date, n_results::Int)::Vector{Tuple{Date, Date}}
     total_days = date2 - date1 + Day(1)
     # Considering the number of cases to be unifromly distributed over the total days.
     # We expect the number of cases to be less than 400 for each query.
     n_queries = Int(ceil(n_results / 400))
-    
+
+    # If the required no of queires are more than the total days,
+    # we need to reduce the number of queries that can be made.
+    # Don't mess with this code, pain in the ass to debug.
     if total_days.value < n_queries
         n_queries = total_days.value
     end
+
     n_days = total_days ÷ n_queries - Day(1)
-    opt_dates = generate_date_with_gaps(date1, date2, n_days, n_queries)
+    opt_dates = divide_dates(date1, date2, n_days, n_queries)
     return opt_dates
+end
+
 end
