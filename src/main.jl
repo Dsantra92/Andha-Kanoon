62af17af83536c4acde139c0caf7f35051aadbc7
Deeptendu Santra <deeptendu.santra@protonmail.com>
2021-11-26 23:39:11 +0530

    support for year-wise download

--- src/main.jl
+++ src/main.jl
@@ -4,6 +4,18 @@ include("years.jl")
 using .years
 using .downloader
 
+
+function download_all_docs(start_year::Int, end_year::Int)
+    court_id_to_years = all_court_id_with_years()
+    for (court_id, years) in pairs(court_id_to_years)
+        for year in years
+            if parse(Int, year) >= start_year && parse(Int, year) <= end_year
+                download_all_docs_for_a_year(court_id, year)
+            end
+        end
+    end
+end
+
 function download_all_docs_for_all_courts()
     court_id_to_years = all_court_id_with_years()
 
